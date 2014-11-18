c04241f0633810ca744a81fa82922696bcbd28ca
Jonathan Kim <jonnokim17@gmail.com>
2014-11-17 21:08:17 -0800

    updated cameraVC

--- InstagramProject/CameraViewController.m
+++ InstagramProject/CameraViewController.m
@@ -7,31 +7,75 @@
 //
 
 #import "CameraViewController.h"
+#import "UploadDetailViewController.h"
+#import "UIImage+Resize.h"
 
-@interface CameraViewController ()
+@interface CameraViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property UIImage *imageToUpload;
 
 @end
 
 @implementation CameraViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self showCameraController];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)showCameraController
+{
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == NO)
+    {
+        // Camera is not available
+        return;
+    }
+    UIImagePickerController *camera = [[UIImagePickerController alloc] init];
+    camera.sourceType = UIImagePickerControllerSourceTypeCamera;
+    camera.allowsEditing = NO;
+    camera.cameraOverlayView = nil;
+    camera.delegate = self;
+
+    [self presentViewController:camera animated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
+//MARK: UIImagePickerControllerDelegate
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *originalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    UIImageWriteToSavedPhotosAlbum(originalImage, nil, nil, nil);
+
+    self.imageToUpload = [originalImage thumbnailImage:750 transparentBorder:0 cornerRadius:0 interpolationQuality:kCGInterpolationDefault];
+
+    // segue to UPLOAD PHOTO VC
+    [self performSegueWithIdentifier:@"toUploadDetailVCSegue" sender:self];
+
+    // If the user returns to this VC from UPLOAD PHOTO VC - this vc will be empty.
+    [self dismissViewControllerAnimated:NO completion:nil];
+}
+
+
+//MARK: Prepare for Segue to Upload Detail VC
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"toUploadDetailVCSegue"])
+    {
+        UploadDetailViewController *uploadPhotoVC = segue.destinationViewController;
+        uploadPhotoVC.imageToUpload = self.imageToUpload;
+    }
+}
+
+
+
 
 @end
--- InstagramProject/UploadDetailViewController.h
+++ InstagramProject/UploadDetailViewController.h
@@ -10,4 +10,6 @@
 
 @interface UploadDetailViewController : UIViewController
 
+@property UIImage *imageToUpload;
+
 @end
